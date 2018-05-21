5a60fb37a1267bfb4be4bffb6adc1a303f137a84
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-21 15:16:54 +0200

    add package stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,4 +10,8 @@ node {
     stage("Integration tests") {
         sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
     }
+    stage("Build artifact") {
+        sh "${mvnHome}/bin/mvn clean package"
+        sh "ls -al"
+    }
 }
