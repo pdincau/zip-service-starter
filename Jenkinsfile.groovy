6a28db42d0af19df38568d8dbd8cc635930c8228
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-22 20:35:52 +0200

    using docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,31 +1,29 @@
 node {
-    def mvnHome = tool "Maven"
-
     stage("Checkout") {
         checkout scm
     }
     stage("Unit tests") {
         try {
-            sh "${mvnHome}/bin/mvn clean test"
+            sh "docker run -v $(pwd):/tmp -w /tmp maven:3.5.3-jdk-8-alpine mvn clean test"
         } finally {
             junit 'target/surefire-reports/**/*.xml'
         }
     }
     stage("Integration tests") {
         try {
-            sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
+            sh "docker run -v $(pwd):/tmp -w /tmp maven:3.5.3-jdk-8-alpine mvn clean test-compile failsafe:integration-test"
         } finally {
             junit 'target/failsafe-reports/**/*.xml'
         }
     }
     stage("Build artifact") {
-        sh "${mvnHome}/bin/mvn clean package"
+        sh "docker run -v $(pwd):/tmp -w /tmp maven:3.5.3-jdk-8-alpine mvn clean package"
     }
     stage("UAT") {
         sh "java -jar -Dhttp.server.port=8082 target/zip-service-jar-with-dependencies.jar &"
         sh "sleep 5"
         try {
-            sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8082 test"
+            sh "docker run -v $(pwd):/tmp -w /tmp maven:3.5.3-jdk-8-alpine mvn -Dtest=\"UAT\" -Dhost=localhost -Dport=8082 test"
         } finally {
             junit 'target/surefire-reports/**/*acceptance*.xml'
         }
