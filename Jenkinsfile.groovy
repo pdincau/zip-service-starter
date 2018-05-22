f8bfa2848d1103b38dc6b876d9c205545941bdfd
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-22 16:33:02 +0200

    add try catch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,21 +5,29 @@ node {
         checkout scm
     }
     stage("Unit tests") {
-        sh "${mvnHome}/bin/mvn clean test"
-        junit 'target/surefire-reports/**/*.xml'
+        try {
+            sh "${mvnHome}/bin/mvn clean test"
+        } finally {
+            junit 'target/surefire-reports/**/*.xml'
+        }
     }
     stage("Integration tests") {
-        sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
-        junit 'target/failsafe-reports/**/*.xml'
+        try {
+            sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
+        } finally {
+            junit 'target/failsafe-reports/**/*.xml'
+        }
     }
     stage("Build artifact") {
         sh "${mvnHome}/bin/mvn clean package"
-        sh "ls -al"
     }
     stage("UAT") {
         sh "java -jar -Dhttp.server.port=8082 target/zip-service-jar-with-dependencies.jar &"
         sh "sleep 5"
-        sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8082 test"
-        junit 'target/surefire-reports/**/*acceptance*.xml'
+        try {
+            sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8082 test"
+        } finally {
+            junit 'target/surefire-reports/**/*acceptance*.xml'
+        }
     }
 }
