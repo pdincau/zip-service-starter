8772ab9c2174add633f8977361baa30f2d94e779
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-22 14:48:23 +0200

    optimize imports

--- src/test/java/zip/ZipServiceTest.java
+++ src/test/java/zip/ZipServiceTest.java
@@ -1,16 +1,15 @@
 package zip;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.Collection;
 import java.util.Collections;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
@@ -29,7 +28,7 @@ public class ZipServiceTest {
                 .isThrownBy(() -> service.dataFor("", ""))
                 .withMessage("One of the parameters is not set");
     }
-
+    
     @Test
     public void returns_zip_info_for_given_country_and_zip_code() {
         ZipInfo aZipInfo = new ZipInfo("40100", "Italia", "it", Collections.EMPTY_LIST);
