97b8bd6c3635f7d31b8bfb8807d436b3403042f7
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-23 12:01:51 +0200

    jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,28 @@
-node {
-    stage("Checkout") {
-        checkout scm
-    }
-    stage("Unit tests") {
-        try {
+try {
+    node {
+        stage("Checkout") {
+            checkout scm
+        }
+        stage("Unit tests") {
             sh "docker run -v /tmp/m2:/root/.m2 -v \$(pwd):/app -w /app maven:3.5.3-jdk-8-alpine mvn clean test"
-        } finally {
-            junit 'target/surefire-reports/**/*.xml'
         }
-    }
-    stage("Integration tests") {
-        try {
+        stage("Integration tests") {
             sh "docker run -v /tmp/m2:/root/.m2 -v \$(pwd):/app -w /app maven:3.5.3-jdk-8-alpine mvn clean test-compile failsafe:integration-test"
-        } finally {
-            junit 'target/failsafe-reports/**/*.xml'
         }
-    }
-    stage("Build artifact") {
-        sh "docker run -v /tmp/m2:/root/.m2 -v \$(pwd):/app -w /app maven:3.5.3-jdk-8-alpine mvn clean package"
-    }
-    stage("UAT") {
-        sh "java -jar -Dhttp.server.port=8082 target/zip-service-jar-with-dependencies.jar &"
-        sh "sleep 5"
-        try {
+        stage("Build artifact") {
+            sh "docker run -v /tmp/m2:/root/.m2 -v \$(pwd):/app -w /app maven:3.5.3-jdk-8-alpine mvn clean package"
+        }
+        stage("UAT") {
+            sh "java -jar -Dhttp.server.port=8082 target/zip-service-jar-with-dependencies.jar &"
+            sh "sleep 5"
             sh "docker run --net host -v /tmp/m2:/root/.m2 -v \$(pwd):/app -w /app maven:3.5.3-jdk-8-alpine mvn -Dtest=\"*UAT\" -Dhost=host.docker.internal -Dport=8082 test"
-        } finally {
-            junit 'target/surefire-reports/**/*acceptance*.xml'
+        }
+        stage("Store Artifact") {
+            archiveArtifacts artifacts: 'target/zip-service-jar-with-dependencies.jar', fingerprint: true
         }
     }
-    stage("Store Artifact") {
-        archiveArtifacts artifacts: 'target/zip-service-jar-with-dependencies.jar', fingerprint: true
-    }
+} finally {
+    junit 'target/surefire-reports/**/*.xml'
+    junit 'target/failsafe-reports/**/*.xml'
+    step( [ $class: 'JacocoPublisher' ] )
 }
