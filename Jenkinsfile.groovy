80fce3e9ad6a52ced4aac560182f9f63c3db7d16
Paolo D'Incau <paolo.dincau@gmail.com>
2018-05-24 09:07:34 +0200

    revert jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,18 +5,10 @@ node {
         checkout scm
     }
     stage("Unit tests") {
-        try {
-            sh "${mvnHome}/bin/mvn clean test"
-        } finally {
-            junit 'target/surefire-reports/**/*.xml'
-        }
+        sh "${mvnHome}/bin/mvn clean test"
     }
     stage("Integration tests") {
-        try {
-            sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
-        } finally {
-            junit 'target/failsafe-reports/**/*.xml'
-        }
+        sh "${mvnHome}/bin/mvn clean test-compile failsafe:integration-test"
     }
     stage("Build artifact") {
         sh "${mvnHome}/bin/mvn clean package"
@@ -24,13 +16,6 @@ node {
     stage("UAT") {
         sh "java -jar -Dhttp.server.port=8082 target/zip-service-jar-with-dependencies.jar &"
         sh "sleep 5"
-        try {
-            sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8082 test"
-        } finally {
-            junit 'target/surefire-reports/**/*acceptance*.xml'
-        }
-    }
-    stage("Store Artifact") {
-        archiveArtifacts artifacts: 'target/zip-service-jar-with-dependencies.jar', fingerprint: true
+        sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8082 test"
     }
 }
